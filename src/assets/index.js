d1889aaaf2cd086c205dcc20f65abd96a4b4ed01
Shashank Umar Vaishy <umarshashank@gmail.com>
2025-07-24 10:42:35 +0530

    added details

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-
+import iitg from "./company/iitg.png"
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
@@ -20,12 +20,13 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-
+import sensible from './Sensible.png'
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
-
+import cc from "./CC-student.jpg";
+import ac from "./CC-admin.jpg";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -56,7 +57,11 @@ export {
   shopify,
   starbucks,
   tesla,
+  iitg,
   carrent,
   jobit,
   tripguide,
+  sensible,
+  cc,
+  ac,
 };
